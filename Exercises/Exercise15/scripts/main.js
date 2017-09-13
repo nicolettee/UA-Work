bbcef43dee497b75b4b85e8911ab7bc478dd6aac
nicolettee <nicolette.taylor@gmail.com>
2017-09-13 12:15:15 -0500

    updates

--- Exercises/Exercise15/scripts/main.js
+++ Exercises/Exercise15/scripts/main.js
@@ -1,10 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-     ///// Code goes here
-
-
-
-
+    ///// Code goes here
+    //Part 1
+    function notify() {
+        alert("clicked");
+    }
+    $("#btnOne").on("click", notify);
+    //Part 2
+    function update() {
+        $(".update-html").html('<h3>Hello Word!</h3>')
+    }
+    $("#btnTwo").on("click", update);
+    //Part 3
+    function text() {
+        $("#text-update").val('Hello Word!')
+    }
+    $("#btnThree").on("click", text);
+    //Part 4
+    function copyHtml() {
+        var copy = $(".copy-html").html();
+        $(".paste-html").html(copy)
+    }
+    $("#btnCopyHtml").on("click", copyHtml);
+    //Part 5
+    function copyValue() {
+        var copyVal = $("#copy-text").val();
+        $("#paste-text").val(copyVal)
+    }
+    $("#btnCopyValues").on("click", copyValue);
 
 
 });
