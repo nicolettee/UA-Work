c16a511baea6da85bc0f4c66dd3bf8b7edd1a542
nicolettee <nicolette.taylor@gmail.com>
2017-09-21 22:00:59 -0500

    Exercise 17 done

---
+++ Exercises/Exercise17/scripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+
+    var frm = $(".form-elements");
+    var ipg = $(".input-group");
+    var fni = $("#firstName");
+    var lni = $("#lastName");
+    var eml = $("#emailAddress");
+
+    function validate() {
+        var input = $(this);
+        var errorMsg = input.closest(".input-group").find(".requiredMsg");
+
+        if (input.val() === "") {
+            errorMsg.fadeIn("slow")
+        } else {
+            errorMsg.fadeOut("slow")
+        }
+    }
+
+    function validEmail() {
+        var input = $(this);
+        var inputVal = input.val()
+        var errorMsg = input.closest(".input-group").find(".emailMsg");
+
+        if (inputVal.indexOf("@") === -1 || inputVal.indexOf(".com") === -1) {
+            errorMsg.fadeIn("slow")
+        } else {
+            errorMsg.fadeOut("slow")
+        }
+    }
+
+    frm.find(".error").hide();
+
+    ipg.filter(":even").addClass("even");
+
+    fni.on("blur", validate);
+
+    lni.on("blur", validate);
+
+    eml.on("blur", validate);
+
+    eml.on("blur", validEmail);
+
+});
