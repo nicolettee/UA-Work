30b95eaedd13bcb42b7fd6144f1814c22529d19c
nicolettee <nicolette.taylor@gmail.com>
2017-09-27 21:51:27 -0500

    remove . to classes

--- Exercises/Exercise18/scripts/fixmycode.js
+++ Exercises/Exercise18/scripts/fixmycode.js
@@ -18,11 +18,11 @@ $(document).ready(function () {
             If our sum is less than 10, add a red class
         */
         if(sum > 10){
-            $('txtThirdNumber').addClass('.blue');
-            $('txtThirdNumber').RemoveClass('.red');
+            $('txtThirdNumber').addClass('blue');
+            $('txtThirdNumber').RemoveClass('red');
         }else{
-            $('txtThirdNumber').addClass('.red');
-            $('txtThirdNumber').RemoveClass('.blue');
+            $('txtThirdNumber').addClass('red');
+            $('txtThirdNumber').RemoveClass('blue');
         }
 
         /*
