884bd5f9cffb97cc1d30524f7701bacfedf728b3
nicolettee <nicolette.taylor@gmail.com>
2017-09-28 13:23:10 -0500

    exercise 18 done

--- Exercises/Exercise18/scripts/fixmycode.js
+++ Exercises/Exercise18/scripts/fixmycode.js
@@ -1,36 +1,37 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     $('#btnAddNumbers').on('click', function () {
         /*
             Retrieve the numbers from the input fields
         */
-        var firstNumber = $('txtFirstNumber').val();
-        var secondNumber = $('txtSecondNumber').val();
-        console.log($('txtFirstNumber').val());
+        console.log($('#txtFirstNumber').val());
+        console.log($('#txtSecondNumber').val());
+               
+        var firstNumber = parseInt($('#txtFirstNumber').val());
+        var secondNumber = parseInt($('#txtSecondNumber').val());
         /*
             Add the two numbers together
         */
-        function sum() {
-           return firstNumber + secondNumber;
-           console.log(sum);
-        };
+        var sum = firstNumber + secondNumber;
+        
+       console.log (sum);
 
         /*
             If our sum is greater than 10. Add a blue class to the input.
             If our sum is less than 10, add a red class
         */
         if(sum > 10){
-            $('txtThirdNumber').addClass('blue');
-            $('txtThirdNumber').removeClass('red');
+            $('#txtThirdNumber').addClass('blue');
+            $('#txtThirdNumber').removeClass('red');
         }else{
-            $('txtThirdNumber').addClass('red');
-            $('txtThirdNumber').removeClass('blue');
+            $('#txtThirdNumber').addClass('red');
+            $('#txtThirdNumber').removeClass('blue');
         }
 
         /*
             Update the third input field with our value.
         */
-        $('#txtThirNumber').val(sum);
+        $('#txtThirdNumber').val(sum);
        
     });
 
